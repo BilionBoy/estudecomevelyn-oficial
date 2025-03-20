6541be7711517628350ea7b07e2d02c8d2618e66
Dhiogoandre <andreaguiar@seduc.ro.gov.br>
2025-03-20 13:54:56 -0400

    refactor: styles and controllers

--- app/models/g_tipo_usuario.rb
+++ app/models/g_tipo_usuario.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 class GTipoUsuario < ApplicationRecord
-  # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
-  # 
-  validates :nome,      presence: true
-  validates :descricao, presence: true
-
   # Callback para converter nome para maiúsculas
   before_save :uppercase_nome
-
+  
+  validates :nome,      presence: true
+  validates :descricao, presence: true
+  
   private
 
   def uppercase_nome
-    self.nome = nome.upcase if nome.present?
+    self.nome = nome.to_s.upcase if nome.present?
   end
 end
