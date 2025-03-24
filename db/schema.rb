0366206282917d0154be7a0a683fac56c6360547
Dhiogoandre <andreaguiar@seduc.ro.gov.br>
2025-03-24 09:46:56 -0400

    refactor: ajustes g_tipo_usuaris

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_20_161519) do
+ActiveRecord::Schema[7.2].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "g_tipo_usuarios", force: :cascade do |t|
-    t.string "nome"
-    t.text "descricao"
-    t.string "created_by"
-    t.string "updated_by"
-    t.datetime "deleted_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
