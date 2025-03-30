bb637c388525fa20eb843dd66c1dbbd9884135e3
Dhiogoandre <dhiogoandre26@gmail.com>
2025-03-30 14:45:40 -0400

    WIP: slugs em g_categoria

--- app/models/g_categoria.rb
+++ app/models/g_categoria.rb
@@ -2,4 +2,16 @@
 
 class GCategoria < ApplicationRecord
   # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
+  belongs_to :segmentos
+
+  validates :nome, presence: true, uniqueness: true
+  validates :slug, presence: true, uniqueness: true
+
+  before_save :generate_slug
+
+  private 
+
+  def generate_slug
+    self.slug = nome.parameterize if slug.blank?
+  end
 end
--- app/models/segmento.rb
+++ app/models/segmento.rb
@@ -2,4 +2,5 @@
 
 class Segmento < ApplicationRecord
   # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
+  has_many :g_categorias, dependent: :destroy
 end
