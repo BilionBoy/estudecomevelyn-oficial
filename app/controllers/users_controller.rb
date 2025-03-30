d5ac616f55f0a4f66ad861054387a0917e3e66cd
Dhiogoandre <dhiogoandre26@gmail.com>
2025-03-30 04:48:51 -0400

    WIP: rotas sidebar e gerenciamento de usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,35 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: %i[show edit update destroy]
+
   def index
+    @users = User.all
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: "Usuário atualizado com sucesso."
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user.destroy
+    redirect_to users_path, notice: "Usuário removido com sucesso."
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :role, :password, :password_confirmation)
   end
 end
