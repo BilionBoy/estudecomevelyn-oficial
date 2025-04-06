999f0cede08b5bb89b3222edc8aef819eee87516
Dhiogoandre <dhiogoandre26@gmail.com>
2025-04-05 20:19:21 -0400

    card de produtos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,26 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :produtos, :cursos, :blog]
+
   def index
+    @g_categorias = GCategoria.all
+
+    if params[:categoria].present?
+      categoria = GCategoria.find_by(slug: params[:categoria])
+
+      if categoria.present?
+        @pagy, @i_produtos = pagy(
+          categoria.i_produtos.where(status: :ativo).order(created_at: :desc),
+          items: 6
+        )
+      else
+        @i_produtos = []
+      end
+    else
+      @pagy, @i_produtos = pagy(
+        IProduto.where(status: :ativo).order(created_at: :desc),
+        items: 6
+      )
+    end
   end
 
   def produtos
