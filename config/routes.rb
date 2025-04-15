006e317cabf8329b974d86fd4cab40dcc00a9fde
Dhiogoandre <andreaguiar@seduc.ro.gov.br>
2025-04-15 08:11:11 -0400

    refactor: promocao produtos  sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  resources :i_promocao_produtos
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
-
+  
   # Admin
   get 'g_admin', to: 'g_admin#index', as: :g_admin
-
+  
   # Público / Store
   root 'store#index'
   get '/papelaria', to: 'store#papelaria', as: :papelaria
   get '/blog',      to: 'store#blog',      as: :blog
   get '/cursos',    to: 'store#cursos',    as: :cursos
-
- 
-
+  
+  
+  
   # Rotas Scaffold
   resources :users, only: %i[index show edit update destroy]
   resources :g_tipo_usuarios
@@ -20,7 +19,8 @@ Rails.application.routes.draw do
   resources :segmentos
   resources :i_produtos
   resources :i_promocoes
-
+  resources :i_promocao_produtos
+  
   # Verificar estado da aplicação
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
