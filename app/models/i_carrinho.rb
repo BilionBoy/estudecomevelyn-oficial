bd234c0d7f6f18e0f7b6db5b5f4a3e9caadd07e2
Dhiogoandre <dhiogoandre26@gmail.com>
2025-04-17 11:21:47 -0400

    validações

--- app/models/i_carrinho.rb
+++ app/models/i_carrinho.rb
@@ -1,4 +1,20 @@
 class ICarrinho < ApplicationRecord
   belongs_to :usuario, class_name: 'User'
   has_many   :i_itens_carrinhos
+
+  validates :status, inclusion: { in: %w[ativo inativo cancelado] }
+
+  
+  def adicionar_item(produto, quantidade)
+    item = i_itens_carrinhos.find_or_initialize_by(i_produto: produto)
+    item.quantidade += quantidade
+    item.preco_unitario = produto.preco
+    item.subtotal = item.quantidade * item.preco_unitario
+    item.save
+  end
+
+  def total
+    i_itens_carrinhos.sum(:subtotal)
+  end
+  
 end
