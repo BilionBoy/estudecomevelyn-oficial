3036ec0fae48411d4803bac5e3ae4ab0ee444261
Dhiogoandre <dhiogoandre26@gmail.com>
2025-05-01 21:19:45 -0400

    refactoor: permissoes cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,7 +10,7 @@ class Ability
     else
       # cliente
       can :read, :all
-      cannot :manage, GTipoUsuario
+      cannot :manage, [GTipoUsuario, IProduto, ICurso, Segmento, GCategoria, IPromocao, IPromocaoProduto]
     end
   end
 end
