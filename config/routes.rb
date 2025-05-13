3b65744da2e4471482677aac4bbe6474d342b33c
Dhiogoandre <andreaguiar@seduc.ro.gov.br>
2025-05-13 09:11:37 -0400

    refactor: checkout and credentials

--- config/routes.rb
+++ config/routes.rb
@@ -12,17 +12,21 @@ Rails.application.routes.draw do
       delete :limpar,               on: :collection, to: 'carrinho#limpar',    as: :limpar
       delete 'remover/:produto_id', on: :collection, to: 'carrinho#remover',   as: :remover_item
     end
-    resource :checkout, only: [:new, :create]
+    resource :checkout, only: [:new,   :create]
     resources :pedidos, only: [:index, :show]
   end
   
   # Links extras da loja
-  get 'carrinho',                       to: 'store/carrinho#show', as: :carrinho
-  get 'papelaria',                      to: 'store#papelaria',     as: :papelaria
-  get 'blog',                           to: 'store#blog',          as: :blog
-  get 'cursos',                         to: 'store#cursos',        as: :cursos
-  get 'store/categorias/:slug',         to: 'store#categoria',     as: :store_categoria
+  get 'carrinho',                       to: 'store/carrinho#show',     as: :carrinho
+  get 'papelaria',                      to: 'store#papelaria',         as: :papelaria
+  get 'blog',                           to: 'store#blog',              as: :blog
+  get 'cursos',                         to: 'store#cursos',            as: :cursos
+  get 'store/categorias/:slug',         to: 'store#categoria',         as: :store_categoria
   
+  get 'checkout/success',               to: 'store/checkouts#success', as: :store_checkout_success
+  get 'checkout/cancel',                to: 'store/checkouts#cancel',  as: :store_checkout_cancel
+
+
   # Admin
   get 'g_admin', to: 'g_admin#index', as: :g_admin
   
@@ -37,7 +41,7 @@ Rails.application.routes.draw do
   resources :i_promocao_produtos
 
   # Robots.txt e Sitemap
-  get '/robots.txt', to: proc { |env| [200, { 'Content-Type' => 'text/plain' }, [File.read(Rails.root.join('public', 'robots.txt'))]] }
+  get '/robots.txt',  to: proc { |env| [200, { 'Content-Type' => 'text/plain' }, [File.read(Rails.root.join('public', 'robots.txt'))]] }
   get '/sitemap.xml', to: proc { |env| [200, { 'Content-Type' => 'application/xml' }, [File.read(Rails.root.join('public', 'sitemap.xml'))]] }
 
   # Health Check
