7d24be01f11f5cad0012e9226c18bac2cf18734d
Dhiogoandre <andreaguiar@seduc.ro.gov.br>
2025-05-28 08:16:20 -0400

    modulo de blog

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -79,6 +79,47 @@ end
   
   
 
+  
   def blog
+    # Buscar categorias de blog
+    @blog_categorias = GBlogCategoria.all.order(:descricao)
+    # Buscar posts com filtros
+    posts_query = GBlogPost.where('data_publicacao <= ?', Time.current)
+    # Filtro por categoria
+    if params[:categoria].present?
+      categoria = GBlogCategoria.find_by(slug: params[:categoria])
+      posts_query = posts_query.where(g_blog_categoria: categoria) if categoria.present?
+    end
+    # Filtro por busca
+    if params[:busca].present?
+      posts_query = posts_query.where(
+        "titulo ILIKE ? OR resumo ILIKE ? OR conteudo ILIKE ?", 
+        "%#{params[:busca]}%", 
+        "%#{params[:busca]}%", 
+        "%#{params[:busca]}%"
+      )
+    end
+    # Filtro por tag (se você implementar tags)
+    if params[:tag].present?
+      # Implementar filtro por tag quando necessário
+    end
+    # Paginação
+    @pagy, @blog_posts = pagy(
+      posts_query.order(data_publicacao: :desc),
+      items: 6
+    )
+    # Posts populares (últimos 3 mais recentes ou por visualizações se implementar)
+    @posts_populares = GBlogPost.where('data_publicacao <= ?', Time.current)
+                                .order(data_publicacao: :desc)
+                                .limit(3)
+    
+    # Tags (se você implementar)
+    @tags = [] # Implementar quando necessário
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
+
 end
--- app/models/g_blog_post.rb
+++ app/models/g_blog_post.rb
@@ -2,7 +2,8 @@
 
 class GBlogPost < ApplicationRecord
   # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
-  belongs_to :g_blog_categoria
+  belongs_to        :g_blog_categoria
+  has_one_attached  :imagem_blog
 
   validates :titulo, :resumo,             presence: true
   validates :titulo, :conteudo,           presence: true
