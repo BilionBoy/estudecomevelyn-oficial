cd4eeb77ff68698f37078089754760dc6c8e61f8
Dhiogoandre <dhiogoandre26@gmail.com>
2025-05-29 13:58:47 -0400

    refactor: corrigindo validacoes das models

--- app/models/g_blog_post.rb
+++ app/models/g_blog_post.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class GBlogPost < ApplicationRecord
-  scope :published, -> { where('data_publicacao <= ?', Time.current) }
-
-  # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
   belongs_to        :g_blog_categoria
   has_one_attached  :imagem_blog
-
-  validates :titulo, :resumo,             presence: true
-  validates :titulo, :conteudo,           presence: true
-  validates :titulo, :g_blog_categoria,   presence: true
-
+  
+  scope :published, -> { where('data_publicacao <= ?', Time.current) }
+  
+  # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
+  validates :titulo,                presence: true
+  validates :resumo,                presence:true
+  validates :conteudo,              presence: true
+  validates :g_blog_categoria_id,   presence: true
 end
