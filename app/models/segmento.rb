e5744fdf553ed3ba434ac7c9218c6bc89aa8b394
Dhiogoandre <andreaguiar@seduc.ro.gov.br>
2025-06-03 09:24:35 -0400

    wip: refactor mdoel

--- app/models/segmento.rb
+++ app/models/segmento.rb
@@ -3,4 +3,7 @@
 class Segmento < ApplicationRecord
   # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
   has_many :g_categorias, dependent: :destroy
+
+  validates :nome,      presence: true, uniqueness: true
+  validates :descricao, presence: true
 end
