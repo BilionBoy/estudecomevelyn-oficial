d41bb545e2d27bc23ca27455f0e5fdab1e5bff61
Dhiogoandre <andreaguiar@seduc.ro.gov.br>
2025-06-04 09:33:28 -0400

    wip: area de clientes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,18 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def set_layout_by_controller
-    if devise_controller?
-      "devise_application"
-    elsif controller_path == "store" || controller_path.start_with?("store/")
-      "home_application"
-    else
-      "application"
-    end
-  end
+ def set_layout_by_controller
+   return "devise_application" if devise_controller?
+ 
+   if current_user&.g_tipo_usuario&.nome == "CLIENTE"
+     "cliente"
+   elsif controller_path == "store" || controller_path.start_with?("store/")
+     "home_application"
+   else
+     "application"
+   end
+ end
+
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || root_path
