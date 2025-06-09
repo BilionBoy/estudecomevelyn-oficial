09079a8516dc454fecc62d57cfcee57bd0c93345
BilionBoy <dhiogoandre26@gmail.com>
2025-06-09 19:26:12 -0400

    wip: migration gblogcategoria

--- db/schema.rb
+++ db/schema.rb
@@ -44,8 +44,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_27_170155) do
 
   create_table "g_blog_categorias", force: :cascade do |t|
     t.string "descricao"
-    t.string "created_by"
-    t.string "updated_by"
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
