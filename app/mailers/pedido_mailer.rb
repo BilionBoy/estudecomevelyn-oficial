87b5303210a1df663decfcf0e127b32ac56ab20f
Dhiogoandre <andreaguiar@seduc.ro.gov.br>
2025-06-09 10:41:59 -0400

    Criando o arquivo do mailer

---
+++ app/mailers/pedido_mailer.rb
@@ -0,0 +1,20 @@
+class PedidoMailer < ApplicationMailer
+  default from: 'sua-loja@seudominio.com' # ou o e-mail verificado no MailerSend
+
+  def enviar_infoprodutos(pedido_id)
+    @pedido = IPedido.find(pedido_id)
+    @usuario = @pedido.usuario
+
+    # Para cada item do pedido, anexa o PDF do produto
+    @pedido.i_itens_pedidos.each do |item|
+      if item.i_produto.arquivo.attached?
+        attachments[item.i_produto.arquivo.filename.to_s] = item.i_produto.arquivo.download
+      end
+    end
+
+    mail(
+      to: @usuario.email,
+      subject: "🎉 Obrigado pela compra! Aqui estão seus infoprodutos 📦"
+    )
+  end
+end
