78ac85d74c458d7c3ff2cb6013f175fa04fbc257
Dhiogoandre <andreaguiar@seduc.ro.gov.br>
2025-06-09 11:09:32 -0400

    wip

--- app/mailers/pedido_mailer.rb
+++ app/mailers/pedido_mailer.rb
@@ -1,11 +1,11 @@
 class PedidoMailer < ApplicationMailer
-  default from: 'sua-loja@seudominio.com' # ou o e-mail verificado no MailerSend
+  default from: 'no-reply@estudecomevelyn.com.br' 
 
   def enviar_infoprodutos(pedido_id)
     @pedido = IPedido.find(pedido_id)
     @usuario = @pedido.usuario
 
-    # Para cada item do pedido, anexa o PDF do produto
+    # Anexa todos os PDFs
     @pedido.i_itens_pedidos.each do |item|
       if item.i_produto.arquivo.attached?
         attachments[item.i_produto.arquivo.filename.to_s] = item.i_produto.arquivo.download
